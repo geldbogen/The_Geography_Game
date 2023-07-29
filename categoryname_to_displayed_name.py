46297e91ee8d215687e5cdb3717b51f26c902457
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2023-07-29 19:52:52 +0200

    displayed name sentences

--- categoryname_to_displayed_name.py
+++ categoryname_to_displayed_name.py
@@ -1,6 +1,13 @@
-# A dictionary which translates the categorynames how they are displayed at the beginning of the game and internally to how they should be displayed during the game
+# A dictionary which translates the categorynames how they are displayed at the beginning of the game 
+# and internally to how they should be displayed during the game
+# BBBB stands for text, which will be displayed in bold, IIII stands for text, to which 'more information' exists 
 
-category_to_displayed_name_dict = {\
-    "Press Freedom Index (more free is better)":"CountryA has more press freedom than CountryB (according to the IIIPress Freedom IndexIII)",\
-    "Year of first KFC opening in that country (lower is better)":"KFC opened their first branch in CountryA earlier, than in CountryB",\
-    "Annual cocaine prevalence (lower is better)":"Less people take cocaine in CountryA than in CountryB"}
+import pandas as pd
+
+df = pd.read_csv("categoryname_to_displayed_sentences.csv")
+df = df.fillna("")
+category_to_displayed_name_dict = df.set_index("original categorynames")["how categorynames should look"].to_dict()
+category_to_displayed_guess_hint = df.set_index("original categorynames")["guessing extra"].to_dict()
+category_to_displayed_extra_information_category = df.set_index("original categorynames")["extra information category"].to_dict()
+
+print(category_to_displayed_guess_hint)
