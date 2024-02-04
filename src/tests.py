0dee9d5f3e24cb6822e942a4887077b2d716ae28
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2024-02-04 15:16:25 +0100

    a

--- src/tests.py
+++ src/tests.py
@@ -13,17 +13,22 @@ from global_definitions import red, yellow
 #     intro_window.go()
 #     return intro_window
 
+
 @pytest.fixture
 def player1():
-    my_player = Player(color=red,name='Player1')
+    my_player = Player(color=red, name='Player1')
     return my_player
+
+
 @pytest.fixture
 def player2():
-    my_player = Player(color=yellow,name='Player2')
+    my_player = Player(color=yellow, name='Player2')
     return my_player
+
 
 def test_countries_are_neighbors():
     # assert 0
-    setup_the_game(continent_list=['Africa'], list_of_players= [player1, player2])
+    setup_the_game(continent_list=['Africa'],
+                   list_of_players=[player1, player2])
     print(Niger.neighboring_countries)
-    assert ('Chad' in Niger.neighboring_countries) == True
+    assert Chad.is_connected_with(Niger) == True
