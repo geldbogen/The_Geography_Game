4fe300530e4d08a2eff716bd0ec66a5046305940
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2025-03-31 14:17:48 +0200

    fix: enhance error handling in country selection logic

--- src/backend_game.py
+++ src/backend_game.py
@@ -13,6 +13,7 @@ if TYPE_CHECKING:
 from global_definitions import all_categories, all_countries_in_game
 from country import Country, Unknown_country
 
+
 class BackendGame():
 
     def __init__(self,
@@ -25,10 +26,10 @@ class BackendGame():
                  pred_attribute: str = "random",
                  peacemode: bool = False,
                  reversed_end_attribute: int = 0):
-        
-        self.list_of_players : list[Player] = list_of_players
-        self.winning_condition :str = winning_condition
-        self.rerolls : int = 3
+
+        self.list_of_players: list[Player] = list_of_players
+        self.winning_condition: str = winning_condition
+        self.rerolls: int = 3
 
         self.number_of_targets = 2
 
@@ -62,32 +63,55 @@ class BackendGame():
         # just declaration
 
         # for claimtwo countries
-        self.targetcountry1 : Country
-        self.targetcountry2 : Country
+        self.targetcountry1: Country
+        self.targetcountry2: Country
 
         # for end attribute
-        self.end_attribute: Category 
-
+        self.end_attribute: Category
 
         # for secret targets
-        self.dict_of_targets : dict[Player,list[Country]] = dict()
+        self.dict_of_targets: dict[Player, list[Country]] = dict()
 
         # for secret attribute
         self.dict_of_target_attribute_name: dict[Player, str] = dict()
-        self.winning_country : Country = Unknown_country
+        self.winning_country: Country = Unknown_country
 
     def get_starting_attribute(self):
         pass
-    
-    def find_top_n_countries(self, n : int, attribute_name : str):
 
-        all_countries_with_data = [country for country in all_countries_in_game if country.dict_of_attributes[attribute_name].rank != -1]
-        all_countries_with_data.sort(key= lambda x: x.dict_of_attributes[attribute_name].rank)
+    def find_top_n_countries(self, n: int, attribute_name: str):
+        """
+        Finds the top N countries based on a specified attribute.
+        This method filters the countries in the game to include only those
+        that have a valid rank (not equal to 0) for the given attribute.
+        It then sorts the countries by their rank for the specified attribute
+        in ascending order and returns the top N countries.
+        Args:
+            n (int): The number of top countries to retrieve.
+            attribute_name (str): The name of the attribute to rank the countries by.
+        Returns:
+            list: A list of the top N countries sorted by the specified attribute's rank.
+        """
+
+        all_countries_with_data = [
+            country for country in all_countries_in_game if country.dict_of_attributes[attribute_name].rank != 0]
+        all_countries_with_data.sort(
+            key=lambda x: x.dict_of_attributes[attribute_name].rank)
         return all_countries_with_data[:n]
-        
-        
-    def reroll(self, activating_player : Player, to_update_category_label: tk.Label,
-               to_update_reroll_button: tk.Button):
+
+    def reroll(self, activating_player: Player, to_update_category_label: tk.Label,
+               to_update_reroll_button: tk.Button) -> None:
+        """
+        Handles the reroll action for a player, allowing them to randomly choose a new current attribute
+        if they have rerolls left. Subtracts one reroll from the player's count and updates the current attribute.
+        Updates the category label and reroll button accordingly.
+        Args:
+            activating_player (Player): The player who is activating the reroll.
+            to_update_category_label (tk.Label): The label widget to update with the new category name.
+            to_update_reroll_button (tk.Button): The button widget to update with the remaining rerolls.
+        Returns:
+            None
+        """
         if activating_player.rerolls_left == 0:
             return None
         activating_player.rerolls_left -= 1
@@ -98,12 +122,34 @@ class BackendGame():
                                                                 )
         to_update_reroll_button.configure(
             text="rerolls left:\n " + str(activating_player.rerolls_left))
+        return None
 
     def check_if_game_should_end(self):
+        """
+        Determines whether the game should end based on the current game state 
+        and the specified winning condition.
+        Winning Conditions:
+            - "claim 2 countries": The game ends if both target countries have 
+              the same owner and the owner's name is not "Nobody".
+            - "get gold": The game ends if the gold list is empty (i.e. all goldcountries have been claimed).
+            # TODO: end game if somebody has the majority of the gold
+            - "secret targets": The game ends if the active player's possesses all of their target countries.
+            - "secret attribute": The game ends if the active player possesses 
+              at least one of their target countries. Additionally, the winning 
+              country is set if a match is found.
+            - Default: If the winning condition is invalid, the game will not end and it will print a warning message.
+        Additional Condition:
+            The game also ends if the active player counter reaches the total 
+            number of turns (calculated as the number of players multiplied by 
+            the number of rounds) minus one.
+        Returns:
+            bool: True if the game should end, False otherwise.
+        """
 
         print('the game is asked to end')
         print('active_player_counter')
         print(self.active_player_counter)
+        
         if self.active_player_counter == len(
                 self.list_of_players) * self.number_of_rounds - 1:
             return True
@@ -118,7 +164,6 @@ class BackendGame():
                 if set(self.dict_of_targets[self.active_player]).issubset(
                         set(self.active_player.list_of_possessed_countries)):
                     return True
-        
             case "secret attribute":
                 if len(
                         set(self.dict_of_targets[self.active_player]).intersection(
@@ -128,19 +173,18 @@ class BackendGame():
                         if country in self.dict_of_targets[self.active_player]:
                             self.winning_country = country
                             return True
-                            
                     return True
             case _:
                 print('illegal winning condition will never end')
                 return False
 
-    def claim_country_backend(self, loose_player : Player, win_player: Player, country : Country):
-        # TODO check docstring
+    def claim_country_backend(self, loose_player: Player, win_player: Player, country: Country):
         """
         Transfers ownership of a country from one player to another.
-        This method handles the backend logic of claiming a country, including updating player's
-        possessed countries list, setting the country's owner, and handling gold acquisition
-        when the winning condition is "get gold".
+        This method handles the backend logic of claiming a country, including:
+        updating player's possessed countries list,
+        setting the country's owner,
+        and handling gold acquisition when the winning condition is "get gold".
         Parameters:
         -----------
         loose_player : Player
@@ -151,22 +195,21 @@ class BackendGame():
             The country being claimed.
         Notes:
         ------
-        - If loose_player's name is not "Nobody" and the winning condition is not "get gold",
+        - If loose_player's name is not "Nobody",
             the country is removed from loose_player's possession and the GUI label is destroyed.
         - If the winning condition is "get gold" and the country contains gold,
             win_player's gold count is incremented and the country is added to their
             list of gold-containing countries.
         """
-        
+
         win_player.list_of_possessed_countries.append(country)
         country.owner_name = win_player.name
-        if loose_player.name != "Nobody" and not self.winning_condition in [
-                "get gold"
-        ]:
+
+        if loose_player.name != "Nobody":
             loose_player.list_of_possessed_countries.remove(
                 country)
             loose_player.labeldict[country].destroy()
-        
+
         if self.winning_condition == "get gold":
             if win_player.name != "Nobody":
                 if country in self.goldlist:
@@ -174,24 +217,21 @@ class BackendGame():
                     self.goldlist.remove(country)
                     win_player.list_of_possessed_countries_gold.append(country)
 
+    def score(self, countrylist: list[Country]) -> list[float]:
 
-    def score(self, countrylist : list[Country]) -> list[float]:
-
-        def helphelp(number : float, list1 : list[float]) -> float:
+        def helphelp(number: float, list1: list[float]) -> float:
             if self.higherorlower == "higher":
                 return float(sum([item <= number for item in list1]))
             if self.higherorlower == "lower":
                 return float(sum([item >= number for item in list1]))
-            
+
             return 0.0
 
-            
-
         # higherorlower : str = ""
-        propertylist : list[float] = list()
-        mcountrylist : list[Country] = list()
-        dlist : list[Country] = list()
-        
+        propertylist: list[float] = list()
+        mcountrylist: list[Country] = list()
+        dlist: list[Country] = list()
+
         if self.reversed_end_attribute == 0:
             if "higher is better" in self.end_attribute.name:
                 self.higherorlower = "higher"
@@ -205,7 +245,7 @@ class BackendGame():
 
         for country in all_countries_in_game:
             try:
-                if (country.dict_of_attributes[self.end_attribute.name].rank != 0 
+                if (country.dict_of_attributes[self.end_attribute.name].rank != 0
                     ) or (self.end_attribute.treat_missing_data_as_bad):
                     propertylist.append(
                         (country.dict_of_attributes[self.end_attribute.name].value))
@@ -217,11 +257,12 @@ class BackendGame():
         print(propertylist)
         print('this is dlist')
         print([d.name for d in dlist])
-        returnlist : list[float] = list()
-        
+        returnlist: list[float] = list()
+
         for country in countrylist:
             if country in dlist:
-                returnlist.append(country.dict_of_attributes[self.end_attribute.name].number_of_countries_ranked // 2)
+                returnlist.append(
+                    country.dict_of_attributes[self.end_attribute.name].number_of_countries_ranked // 2)
             else:
                 returnlist.append(
                     helphelp(
@@ -229,21 +270,21 @@ class BackendGame():
                         propertylist))
         returnlist = [float(item) / float(5) for item in returnlist]
         return returnlist
-    
+
     def get_two_countries_for_wormhole_connection(self, player: Player = None) -> tuple[Country, Country]:
         """
         Gets two countries for a wormhole connection that meet specific criteria.
-        
+
         This method selects two countries that:
         - Are not neighbors
         - Are from different continents
         - If player is provided, one country MUST NOT be owned by the player
         - Neither country is the Unknown_country
         - In peace mode, at least one country must be unowned
-        
+
         Args:
             player (Player, optional): The player for whom to create a wormhole connection.
-                If None, default countries (Germany and France) will be returned without validation.
+                If None, default countries will be returned without validation.
 
         Returns:
             tuple[Country, Country]: A pair of countries suitable for creating a wormhole connection.
@@ -252,18 +293,18 @@ class BackendGame():
         country2 = Unknown_country
         if player != None:
             while (
-                    country2.name in country1.neighboring_countries
-                    or country1.name in country2.neighboring_countries
-                    or country1.continent_name == country2.continent_name
-                    or country2 in player.list_of_possessed_countries
-                    or country1 == Unknown_country
-                    or country2 == Unknown_country 
-                    or
-                (self.peacemode and
-                #  TODO in case of end_attribute don't connect with Mr_Nobody country
-                 (country1.owner_name != "Nobody" and country2.owner_name != "Nobody"))
-                ):
-                
+                country2.name in country1.neighboring_countries
+                or country1.name in country2.neighboring_countries
+                or country1.continent_name == country2.continent_name
+                or country2 in player.list_of_possessed_countries
+                or country1 == Unknown_country
+                or country2 == Unknown_country
+                or
+                    (self.peacemode and
+                     #  TODO in case of end_attribute don't connect with Mr_Nobody country
+                     (country1.owner_name != "Nobody" and country2.owner_name != "Nobody"))
+            ):
+
                 country1 = random.choice(player.list_of_possessed_countries)
                 country2 = random.choice(all_countries_in_game)
         return country1, country2
