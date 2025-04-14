39417032d7cbab2ba4fb39d4a25a48335de9f3b4
jniemeyer_az_adm <jniemeyer_az_adm@gasag.de>
2025-04-14 14:28:02 +0200

    a

--- src/Start.py
+++
@@ -1,8 +0,0 @@
-import setup_data as sd
-from help_functions import save_properties
-from intro_window import IntroWindow
-
-if __name__ == '__main__':
-    sd.setup_all_data()
-    save_properties()
-    IntroWindow()
