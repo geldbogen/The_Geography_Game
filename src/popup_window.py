a129524ba10161dea112c4d5e97bccbe5be845ab
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2025-07-26 12:07:31 +0200

    refactor: rename pop_up_window to pop_up_window_content for clarity and improve modal structure

--- src/popup_window.py
+++ src/popup_window.py
@@ -6,8 +6,24 @@ from dash_extensions.javascript import Namespace, assign
 
 import dash_bootstrap_components as dbc
 
-pop_up_window = html.Div([
+pop_up_window_content = html.Div([
     html.H2("Country Information"),
     html.Div(id="country-info"),
     dbc.Button("Close", id="close-button")
 ])
+
+popup_window = dbc.Modal(
+    children=pop_up_window_content,
+    id="popup-window",
+    is_open=False,
+)
+
+@callback(
+    Output("popup-window", "is_open"),
+    Input("close-button", "n_clicks"),
+    State("popup-window", "is_open"),
+)
+def toggle_popup(n_clicks, is_open):
+    if n_clicks:
+        return not is_open
+    return is_open
