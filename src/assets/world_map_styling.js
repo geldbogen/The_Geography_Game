a1ad7f77c8416505b18f4ff212097864d967343d
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2025-07-26 12:30:57 +0200

    feat: add pulsing effect for selected countries on the world map

--- src/assets/world_map_styling.js
+++ src/assets/world_map_styling.js
@@ -3,7 +3,11 @@ window.myNamespace = Object.assign({}, window.myNamespace, {
         my_style: function(feature, context) {  
             const player_color_dict = context.hideout.player_color_dict;
             const country_owner_dict = context.hideout.country_owner_dict;
+            const selected = context.hideout.selected;
             const country_name = feature.properties.name;
+            if(selected.includes(country_name)){
+                return {fillColor: 'yellow', color: 'yellow', className : 'pulsing-country'}
+            }
 
             if(Object.keys(country_owner_dict).includes(country_name)){
                 const country_owner = country_owner_dict[country_name];
